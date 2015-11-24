9efea8da5f605fe2675f5f3563f9e47e3b13c309
quan32 <ntquan3291@gmail.com>
2015-11-25 00:36:23 +0900

    Initial Commit

---
+++ C++/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  C++
+//
+//  Created by Nguyen Trung Quan on 2015/11/25.
+//  Copyright © 2015年 Nguyen Trung Quan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
